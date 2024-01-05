99d3d53ec65ee11ced456dd27a056f811b6ce7b7
Matt Holubinka <meholubinka@gmail.com>
2024-01-05 13:59:40 +0000

    test poetry commit hooks

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import logging.config
 from logging import Logger, getLogger
 
-from app.common.constants import APP_LOGGER_NAME
-from app.common.logging import config
+from common.constants import APP_LOGGER_NAME
+from common.logging import config
 
 logging.config.dictConfig(config)
 logger: Logger = getLogger(APP_LOGGER_NAME)
 
-print("Hello World!.")
+print("Hello World!")
